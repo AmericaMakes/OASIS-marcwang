3a45976e0d154d7b40a8a9c62fe32cdcac45c2ca
marc wang <marc.wang@uwaterloo.ca>
2021-01-25 11:28:26 -0500

    bisector
    finished bisector

--- oasis/skeleton.py
+++ oasis/skeleton.py
@@ -4,50 +4,63 @@ from numpy import linalg as LA
 
 from shapely.geometry.polygon import Polygon, orient
 from shapely.coords import CoordinateSequence
-from shapely.geometry import MultiLineString
 from trimesh.creation import triangulate_polygon
 from trimesh.path.exchange.load import load_path
+
+from trimesh.path.simplify import merge_colinear
 
 
 class Skeletonization():
     def __init__(self, polygon: Polygon) -> None:
-        self.poly = orient(polygon)
+        precision = 1e-3
+        #remove colinear segment
+        outer = merge_colinear(polygon.exterior.coords, scale=precision)
+        inners = [merge_colinear(hole.coords, scale=precision)
+                  for hole in polygon.interiors]
+        simplified_poly = Polygon(outer, holes=inners)
+        
+        self.poly = orient(simplified_poly)
+
         self._mesh_v, self._mesh_f = triangulate_polygon(
-            polygon, engine='earcut')
+            self.poly, engine='earcut')
 
-        self.inner = self.compute_bisector(self.poly.exterior.coords)
-        self.outer = [self.compute_bisector(
-            hole.coords, False) for hole in self.poly.interiors]
+        self.shell_bisector = self.compute_bisector(self.poly.exterior.coords)
+        self.holes_bisector = [self.compute_bisector(
+            hole.coords) for hole in self.poly.interiors]
 
-    def compute_bisector(self, poly_v: CoordinateSequence, ccw: bool = True):
+    def compute_bisector(self, poly_v: CoordinateSequence, holes: bool = False):
         vert_i = np.array(poly_v)
-        vert_i_1 = np.roll(vert_i, shift=1, axis=0)
-        vec_a = vert_i_1 - vert_i
+        vec_a = vert_i[1:] - vert_i[0:-1]
         vec_b = -1*np.roll(vec_a, shift=1, axis=0)
         direction = np.cross(vec_a, vec_b, axis=1)
 
-        if (ccw):
-            dir_sign = direction <= 0
-        else:
-            dir_sign = direction > 0
+        # assume no colinear segment pair
+        dir_sign = direction >= 0
 
+        # flip direction for angle bigger than 180
         sign = np.where(dir_sign, np.ones(
             direction.shape[0]),  -1*np.ones(direction.shape[0]))[:, np.newaxis]
         bisector = LA.norm(vec_b, axis=1)[
             :, np.newaxis]*vec_a + LA.norm(vec_a, axis=1)[:, np.newaxis]*vec_b
 
         return sign*bisector/LA.norm(bisector, axis=1)[:, np.newaxis]
-    
-    def show_velocity(self):
+
+    def show_velocity_outer(self):
         polygon = load_path(self.poly)
-        vert_1 = np.array(self.poly.exterior.coords)
-        vert_2 = vert_1 + self.inner
-        
-        shapely_line = MultiLineString(np.nan_to_num(np.stack((vert_1, vert_2), axis=2)).tolist())
+        vert_1 = np.array(self.poly.exterior.coords)[:-1]
+        vert_2 = vert_1 + self.shell_bisector
+        lines = np.nan_to_num(np.stack((vert_1, vert_2), axis=1))
+        shapely_lines = load_path(lines)
 
-        lines = load_path(shapely_line)
+        (shapely_lines + polygon).show()
 
-        (lines + polygon).show()
+    def show_velocity_inner(self, idx: int = 0):
+        polygon = load_path(self.poly)
+        vert_1 = np.array(self.poly.interiors[idx].coords)[:-1]
+        vert_2 = vert_1 + self.holes_bisector[idx]
+        lines = np.nan_to_num(np.stack((vert_1, vert_2), axis=1))
+        shapely_lines = load_path(lines)
+        (shapely_lines + polygon).show()
 
     def show_mesh(self):
         fig = plt.figure()
--- test/test_skeleton.py
+++ test/test_skeleton.py
@@ -5,6 +5,7 @@ from oasis.skeleton import Skeletonization
 
 import numpy as np
 
+
 class SkeletonizationTest(unittest.TestCase):
 
     def setUp(self):
@@ -12,18 +13,18 @@ class SkeletonizationTest(unittest.TestCase):
 
         obj = trimesh.load(stl_path)
 
-        height = (0,0,10)
-        sec = obj.section(plane_origin=height, 
-                            plane_normal=[0,0,1])
+        height = (0, 0, 10)
+        sec = obj.section(plane_origin=height,
+                          plane_normal=[0, 0, 1])
 
         slice_2d, _ = sec.to_planar()
 
         self.polygons_2d = slice_2d.polygons_full
 
-
     def test_generate_bisector(self):
         skel = Skeletonization(self.polygons_2d[0])
-        skel.show_velocity()
+        skel.show_velocity_outer()
+        skel.show_velocity_inner(0)
 
 
 if __name__ == '__main__':
