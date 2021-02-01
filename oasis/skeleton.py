74f714ff0a711f8698fc8aa665bf324d073ff971
marc wang <marc.wang@uwaterloo.ca>
2021-01-31 19:05:53 -0500

    moved to graph

--- oasis/skeleton.py
+++ oasis/skeleton.py
@@ -13,12 +13,19 @@ from typing import Tuple, List
 from numba import jit, prange
 
 from enum import IntEnum
+import heapq as hq
+
+import networkx as nx
+
 
 class TriEvent(IntEnum):
     No = 0
     Edge = 1
     Split = 2
     Flip = 3
+    Multi = 4
+    Removed = 5
+
 
 @jit(nopython=True)
 def compute_edge_length(t: float, v_1: np.array, v_2: np.array, vel_1: np.array, vel_2: np.array) -> float:
@@ -47,26 +54,26 @@ def edge_collapse(tri: np.array, vel: np.array) -> Tuple[float, Tuple[int, int]]
             if length < 1e-3:
                 edge_t[i] = min_t
                 edge_collapse.append((min_t, p))
-    
+
     min_idx = np.argmin(edge_t)
     return (edge_t[min_idx], pair_edges[min_idx])
 
 
-#@jit(nopython=True)
+@jit(nopython=True)
 def vertex_collision(vert_vel: np.array, edges: np.array, wavefront: np.array) -> Tuple[float, int, int]:
     edge_t = np.array([np.inf, np.inf, np.inf])
-    event_type = np.array([0,0,0])
+    event_type = np.array([0, 0, 0])
     for i in range(3):
         vert_v_i = vert_vel[i, ...]
         adj_e = edges[i-1]
-        
+
         j = (i + 1) % 3
         op_e = edges[j, ...]
 
         if wavefront[j]:
             # assume tri is always ccw ordered
             dir = np.array((0.0, 0.0, - 1.0))
-            #perp points inward
+            # perp points inward
             perp_dir = np.cross(op_e, dir)
             opp_e_vel = (perp_dir/LA.norm(perp_dir))[0:2]
         else:
@@ -76,14 +83,20 @@ def vertex_collision(vert_vel: np.array, edges: np.array, wavefront: np.array) -
             np.dot(op_e, op_e)*op_e
 
         rel_speed = opp_e_vel - vert_v_i
-        t = np.dot(dist, dist)/np.dot(rel_speed, dist)
+
+        perp_speed = np.dot(rel_speed, dist)
+
+        if perp_speed != 0:
+            t = np.dot(dist, dist)/perp_speed
+        else:
+            t = 0
 
         if t > 0 and wavefront[j]:
             edge_t[i] = t
-            event_type[i] = TriEvent.Split # split
+            event_type[i] = TriEvent.Split  # split
         elif t > 0:
             edge_t[i] = t
-            event_type[i] = TriEvent.Flip # flip
+            event_type[i] = TriEvent.Flip  # flip
 
     min_idx = np.argmin(edge_t)
     return (edge_t[min_idx], min_idx, event_type[min_idx])
@@ -107,50 +120,112 @@ def compute_edge_velocity(vert: np.array, holes: bool = False) -> np.array:
     return np.concatenate((normed_perp, np.expand_dims(normed_perp[0], axis=0)), axis=0)
 
 
-@jit(nopython=True)
+# @jit(nopython=True)
 def compute_vertex_velocity(edge_vel: np.array) -> np.array:
     # assume last is identical to first
-    edge_vel = edge_vel[0:-1, ...] + edge_vel[1:, ...]
+    edge_vel = np.roll(edge_vel[0:-1, ...] +
+                       edge_vel[1:, ...], shift=1, axis=0)
     # add first to last
     return np.concatenate((edge_vel, np.expand_dims(edge_vel[0], axis=0)), axis=0)
 
-def compute_wavefront(vert : np.array, vel_vert : np.array, mesh : np.array, poly_loop : List[Tuple[int, int, int]]):
+def _check_if_node_last(g : nx.DiGraph, v1_idx : int, v2_idx : int, poly_loop: List[Tuple[int, int, int]]):
+    if not g.has_node(v1_idx):
+        l_id = g.nodes[v2_idx]['l_id']
+        return poly_loop[l_id][0]
+    else:
+        return v1_idx
+
+def create_graph(mesh_v: np.array, vel_v: np.array, mesh_f: np.array, poly_loop: List[Tuple[int, int, int]]):
+    g = nx.DiGraph()
+    m_g = nx.Graph()
+    for l_id, loop in enumerate(poly_loop):
+        interval = range(loop[0], loop[1])
+        hole = loop[2]
+        ls_nodes = [(i, {'coord': mesh_v[i, ...],
+                         'velocity': vel_v[i, ...], 'l_id': l_id}) for i in interval]
+        g.add_nodes_from(ls_nodes)
+
+        ls_edges = [(i, (i+1) % loop[1] + loop[0]*(int((i+1)/loop[1])),
+                     {'hole': hole, 'wave': True, 'l_id': l_id}) for i in interval]
+        g.add_edges_from(ls_edges)
+
+    for t_idx, tri in enumerate(mesh_f):
+        m_g.add_node(t_idx)
+        n_tri_v = np.zeros(3, dtype = int)
+        for i in range(3):
+            v_1_id = tri[i]
+            v_2_id = tri[(i+1) % 3]
+
+            v_1_id = _check_if_node_last(g, v_1_id, v_2_id, poly_loop)
+            v_2_id = _check_if_node_last(g, v_2_id, v_1_id, poly_loop)
+
+            if g.has_edge(v_1_id, v_2_id):
+                g[v_1_id][v_2_id]['t_id'] = t_idx
+            else:
+                g.add_edge(v_1_id, v_2_id)
+                g[v_1_id][v_2_id]['wave'] = False
+                g[v_1_id][v_2_id]['t_id'] = t_idx
+
+            if g.has_edge(v_2_id, v_1_id):
+                if not g[v_2_id][v_1_id]['wave']:
+                    adj_t_idx = g[v_2_id][v_1_id]['t_id']
+                    m_g.add_edge(t_idx, adj_t_idx)
+            
+            n_tri_v[i] = v_1_id
+        m_g.nodes[t_idx]['vertex'] = n_tri_v
+
+    return g, m_g
+
+def compute_wavefront(g_v : nx.DiGraph, g_m : nx.Graph):
     collapse_heap = []
-    for tri in mesh:
-        tri_v = vert[tri]
-        tri_v_vel = vert[tri]
+    event_list = []
 
-        shift_idx = np.roll(tri, shift=-1)
-        diff = np.abs(shift_idx - tri)
+    for tri_idx in g_m.nodes:
+        v_idx = g_m[tri_idx]['vertex']
+        tri_v = np.array([g_v[v]['coord'] for v in v_idx]) 
+        tri_v_vel = np.array([g_v[v]['velocity'] for v in v_idx])
 
-        wavefront = np.zeros_like(tri, dtype = bool)
-        wavefront[diff == 1] = True
-        non_sequential = np.argwhere(wavefront != 1)
+        tri_edge = np.roll(tri_v, shift=-1, axis=0) - tri_v
+        wavefront = np.array([g_v[v_idx[i]][v_idx[(i + 1) % 3]]['wave'] for i in range(3)])
 
-        for idx in non_sequential:
-            idx_2 = shift_idx[idx]
-            idx_1 = tri[idx]
-            for loop in poly_loop:
-                idx_range = range(loop[0], loop[1])
-                if idx_2 in idx_range and idx_1 in idx_range:
-                    if np.abs(idx_2 - idx_1) == (loop[1]-2 - loop[0]):
-                        wavefront[idx] = True
-                    else:
-                        wavefront[idx] = False
-        tri_edge = np.roll(tri_v, shift = -1, axis = 0) - tri_v
         e_collapse = edge_collapse(tri_v, tri_v_vel)
         v_collision = vertex_collision(tri_v_vel, tri_edge, wavefront)
 
-        if e_collapse[0] > v_collision[0]:
-            edge_id = e_collapse[1]
-            v_id_1 =  tri[edge_id]
-            v_id_2 = shift_idx[edge_id]
-            collapse_heap.append((e_collapse[0], (1, (v_id_1, v_id_2) )))
-        elif e_collapse[0] < v_collision[0]:
-            v_id = tri[e_collapse[1]]
-            collapse_heap.append((v_collision[0], (e_collapse[2], v_id)))
-    
-    return  collapse_heap    
+        if e_collapse[0] < v_collision[0]:
+            v_pair_id = e_collapse[1]
+            hq.heappush(collapse_heap, (e_collapse[0], tri_idx))
+            event_list.append((TriEvent.Edge, v_pair_id))
+        elif e_collapse[0] > v_collision[0]:
+            v_id = v_idx[v_collision[1]]
+            hq.heappush(collapse_heap, (v_collision[0], tri_idx))
+            event_list.append((v_collision[2], v_id))
+        else:
+            if e_collapse[0] == np.inf and v_collision[0] == np.inf:
+                hq.heappush(collapse_heap, (np.inf, tri_idx))
+                event_list.append((TriEvent.No,))
+            else:
+                edge_id = e_collapse[1]
+                v_id = v_idx[v_collision[1]]
+                hq.heappush(collapse_heap, (e_collapse[0], tri_idx))
+                event_list.append(
+                    (TriEvent.Multi, ((TriEvent.Edge, edge_id), (v_collision[2], v_id))))
+
+    processed_event = []  # (tri left, t collapse, sorted tri_idx)
+
+    while len(collapse_heap) > 0:
+        elem = hq.heappop(collapse_heap)
+        event = event_list[elem[1]]
+        tri_idx = g_m[elem[1]]['vertex']
+
+        if event[0] == TriEvent.Edge:
+            v_pair = event[1]
+            g_v = nx.contracted_edge(g_v, v_pair)
+            
+        elif event[0] == TriEvent.Split:
+            pass
+
+    return collapse_heap
+
 
 class Skeletonization():
     def __init__(self, polygon: Polygon) -> None:
@@ -192,7 +267,6 @@ class Skeletonization():
     def compute_wavefront(self, step: float = 0.1):
         self.mesh_v, self.mesh_f = triangulate_polygon(
             self.poly, engine='earcut')
-
 
     def show_mesh(self):
         fig = plt.figure()
--- test/test_skeleton.py
+++ test/test_skeleton.py
@@ -1,7 +1,7 @@
 import unittest
 import trimesh
 
-from oasis.skeleton import compute_edge_velocity, compute_vertex_velocity, edge_collapse, vertex_collision, compute_wavefront, TriEvent
+import oasis.skeleton as skel
 from trimesh.creation import triangulate_polygon
 from shapely.geometry import Polygon
 import numpy as np
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import geopandas as gpd
 
-import heapq as hq
+import networkx as nx
 
 
 def plot_mesh(mesh_v, mesh_f):
@@ -62,12 +62,32 @@ class SkeletonizationTest(unittest.TestCase):
 
     def test_generate_bisector(self):
         square = Polygon([(0,0), (5,0), (5,5), (0,5)])
-        e_vel = compute_edge_velocity(np.array(square.exterior.coords))
-        vel = compute_vertex_velocity(e_vel)
+        e_vel = skel.compute_edge_velocity(np.array(square.exterior.coords))
+        vel = skel.compute_vertex_velocity(e_vel)
         self.assertTrue(np.all(vel[0] == np.array([1,1])))
         self.assertTrue(np.all(vel[1] == np.array([-1,1])))
         self.assertTrue(np.all(vel[2] == np.array([-1,-1])))
         self.assertTrue(np.all(vel[3] == np.array([1,-1])))
+    
+    def test_generate_graph(self):
+        outer = [(0,0), (5,0), (5,5), (0,5)]
+        hole = [(1,1), (1,3), (4,3), (4,1)] 
+        square = Polygon(outer, holes= [hole])
+
+        mesh_v, mesh_f = triangulate_polygon(
+            square, engine='earcut')
+
+        plot_mesh(mesh_v, mesh_f)
+        plt.show()
+
+        vel_v = np.zeros((10, 3), dtype = float)
+        poly_l = [(0, 4, 0), (5, 9, 1)]
+
+        graph_v, graph_m = skel.create_graph(mesh_v, vel_v, mesh_f, poly_l)
+        fig, ax = plt.subplots(2,1, figsize = (10,10))
+        nx.draw(graph_v,with_labels=True, ax = ax[0])
+        nx.draw(graph_m,with_labels=True, ax = ax[1])
+        plt.show()
 
     def test_edge_collapse(self):
         tri = np.array([
@@ -82,7 +102,7 @@ class SkeletonizationTest(unittest.TestCase):
             [0, 0]], dtype=float
         )
 
-        test = edge_collapse(tri, vel)
+        test = skel.edge_collapse(tri, vel)
         self.assertEqual(test[0], 2.0)
         self.assertEqual(test[1][0], 0)
         self.assertEqual(test[1][1], 1)
@@ -102,11 +122,11 @@ class SkeletonizationTest(unittest.TestCase):
             [0, -1]], dtype=float
         )
 
-        test = vertex_collision(vel, edges, wavefront)
+        test = skel.vertex_collision(vel, edges, wavefront)
 
         self.assertEqual(test[0], 2.5)
         self.assertEqual(test[1], 2)
-        self.assertEqual(test[2], TriEvent.Split)
+        self.assertEqual(test[2], skel.TriEvent.Split)
 
     def test_vertex_flip_collision(self):
         tri = np.array([
@@ -124,11 +144,11 @@ class SkeletonizationTest(unittest.TestCase):
             [0, -1]], dtype=float
         )
 
-        test = vertex_collision(vel, edges, wavefront)
+        test = skel.vertex_collision(vel, edges, wavefront)
 
         self.assertEqual(test[0], 5.0)
         self.assertEqual(test[1], 2)
-        self.assertEqual(test[2], TriEvent.Flip)
+        self.assertEqual(test[2], skel.TriEvent.Flip)
 
     def test_compute_wavefront(self):
         poly = Polygon([(3.0, 0.0), (3.5, 1.5), (4.0, 0.0),
@@ -136,15 +156,21 @@ class SkeletonizationTest(unittest.TestCase):
         # plt.show()
         mesh_v, mesh_f = triangulate_polygon(
             poly, engine='earcut')
+            
+        t_mesh = trimesh.Trimesh(mesh_v, mesh_f)
+        t_mesh.show()
 
-        poly_loop = [(0, 9, 0)]
-        edge_vel = compute_edge_velocity(mesh_v)
-        vert_vel = compute_vertex_velocity(edge_vel)
+        poly_loop = [(0, 8, 0)]
+        edge_vel = skel.compute_edge_velocity(mesh_v)
+        vert_vel = skel.compute_vertex_velocity(edge_vel)
 
-        plot_mesh(mesh_v, mesh_f)
-        plt.show()
-        heap_list = compute_wavefront(mesh_v, vert_vel, mesh_f, poly_loop)
-        print(heap_list)
+        g_v, g_m = skel.create_graph(mesh_v, vert_vel, mesh_f, poly_loop)
+
+        print(g_v)
+        #plot_mesh(mesh_v, mesh_f)
+        #plt.show()
+        #heap_list = skel.compute_wavefront(g_v, g_m)
+        #print(heap_list)
 
 
 if __name__ == '__main__':
