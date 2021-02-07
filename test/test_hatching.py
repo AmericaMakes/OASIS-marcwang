41edfada4560636eb33574e604bece37e1be0ac0
marc wang <marc.wang@uwaterloo.ca>
2021-02-07 15:58:35 -0500

    commit code
    branch out to include voro++

--- test/test_hatching.py
+++ test/test_hatching.py
@@ -11,22 +11,30 @@ class SkeletonizationTest(unittest.TestCase):
     def setUp(self):
         stl_path = './test/test_artifact/3DBenchy.stl'
 
-        obj = trimesh.load(stl_path)
+        self.obj = trimesh.load(stl_path)
 
         height = (0, 0, 10)
-        sec = obj.section(plane_origin=height,
+        sec = self.obj.section(plane_origin=height,
                           plane_normal=[0, 0, 1])
 
         slice_2d, _ = sec.to_planar()
 
         slice_2d.merge_vertices()
         self.polygons_2d = [translate(poly, xoff=25, yoff=20) for poly in slice_2d.polygons_full]
-        
+    
+    def test_pt_sampler(self):
+        sampler = ohat.PointSampler(self.obj, 10)
+        pt = sampler.generate_point(1000)
+        pt_c = trimesh.points.PointCloud(pt, colors='blue')
+        pt_c.show()
+    
+    def test_voronoi(self):
+        v_infill = ohat.VoronoiInfill(self.obj, 10)
+        v_infill.get_slice(10)
     
     def test_hatching(self):
-        hat = ohat.Hatching(0)
         cleaned_poly = clean_polygon(self.polygons_2d[0])
-        trimmed_hatch = hat.straight_hatch(cleaned_poly, 0.1, 45)
+        trimmed_hatch = ohat.straight_hatch(cleaned_poly, 0.1, 45)
 
         poly_path = trimesh.load_path(cleaned_poly)
         poly_path += trimesh.load_path(trimmed_hatch)
