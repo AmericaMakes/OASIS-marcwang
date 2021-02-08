b3ab5835c5158a4585d48699ee8e39df13ef2a77
marc wang <marc.wang@uwaterloo.ca>
2021-02-08 14:02:59 -0500

    basic compilation

--- test/test_voronoi.cpp
+++ test/test_voronoi.cpp
@@ -1,10 +1,9 @@
 
-#include "catch.hpp"
-
+#include <catch2/catch.hpp>
 #include "voronoi_generator.h"
 
 TEST_CASE( "test voronoi gen", "[test_voronoi.cpp]" )
 {
     std::shared_ptr<VoronoiHatcher> vor;
-    REQUIRE( vor != nullptr );s
+    REQUIRE( vor != nullptr );
 }
