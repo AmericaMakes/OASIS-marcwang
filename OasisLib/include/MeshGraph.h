4d7edaccf88775807afafb378c2b40b754d73684
marc wang <marc.wang@uwaterloo.ca>
2021-02-23 18:58:05 +0000

    store halfedge

--- OasisLib/include/MeshGraph.h
+++ OasisLib/include/MeshGraph.h
@@ -3,20 +3,22 @@
 
 #include <boost/geometry.hpp>
 #include <boost/geometry/index/rtree.hpp>
+ #include <boost/geometry/geometries/register/point.hpp> 
+
 #include <geogram/basic/common.h>
 #include <geogram/mesh/mesh.h>
+#include <geogram/mesh/mesh_halfedges.h>
 #include <geogram/basic/numeric.h>
 
 using namespace GEO;
 namespace bg = boost::geometry;
 namespace bgi = boost::geometry::index;
 
-using pt = bg::model::point<float, 3, bg::cs::cartesian>;
+using pt = vecng<3, double>;
+BOOST_GEOMETRY_REGISTER_POINT_3D(pt, double, bg::cs::cartesian, data()[0], data()[1], data()[2]);
 using c_range = bg::model::box<pt>;
-using value = std::pair<c_range, index_t>;
+using value = std::pair<c_range, MeshHalfedges::Halfedge>;
 using rtree = bgi::rtree<value, bgi::quadratic<16> >;
-using cell_id_map = std::map<index_t, std::vector<index_t> >;
-using edge_pair = std::pair<vecng<3, double>,vecng<3, double>>;
 
 namespace OasisLib{
 
@@ -26,8 +28,8 @@ namespace OasisLib{
             size_t nb_nodes;
             MeshHeightSlicer(std::shared_ptr<Mesh> m_ptr);
             vector<value> get_layer(double z);
-            void clip_cell(vector<value> &target_facet, Mesh &m_out, double z);
-            edge_pair clip_cell_by_plane(index_t f, const Mesh *mesh, double z);
+            //void clip_cell(vector<value> &target_facet, Mesh &m_out, double z);
+            //edge_pair clip_cell_by_plane(index_t f, const Mesh *mesh, double z);
 
         protected:
             rtree mesh_tree;
--- OasisLib/src/MeshGraph.cpp
+++ OasisLib/src/MeshGraph.cpp
@@ -14,40 +14,43 @@ namespace OasisLib
 
     void MeshHeightSlicer::initialize_rtree()
     {
-        index_t nb_cell = this->mesh_ptr->cells.nb();
-
-        for (index_t c_i = 0; c_i < nb_cell; ++c_i)
+        MeshHalfedges mhl(*this->mesh_ptr);
+        
+        index_t nb_f = this->mesh_ptr->facets.nb();
+        for (index_t f_i = 0; f_i < nb_f; ++f_i)
         {
-            std::array<double, 3> min_v;
-            min_v.fill(std::numeric_limits<float>::infinity());
-
-            std::array<double, 3> max_v;
-            max_v.fill(-std::numeric_limits<float>::infinity());
-            index_t nb_v = this->mesh_ptr->cells.nb_vertices(c_i);
-            for (index_t v_i = 0;
-                 v_i < nb_v; ++v_i)
+            for (auto c_i = this->mesh_ptr->facets.corners_begin(f_i);
+                c_i < this->mesh_ptr->facets.corners_end(f_i); ++c_i)
             {
-                index_t g_index = this->mesh_ptr->cells.vertex(c_i, v_i);
-                auto v_pos = this->mesh_ptr->vertices.point(g_index);
+                MeshHalfedges::Halfedge hl(f_i, c_i);
 
-                for (index_t i = 0; i < 3; i++)
+                auto p_1_id = this -> mesh_ptr->facet_corners.vertex(c_i);
+                auto c_i_1 = this -> mesh_ptr->facets.next_corner_around_facet(f_i, c_i);
+                
+                auto p_2_id = this -> mesh_ptr->facet_corners.vertex(c_i_1);
+
+                auto p_1 = this -> mesh_ptr->vertices.point(p_1_id);
+                auto p_2 = this -> mesh_ptr->vertices.point(p_2_id);
+                
+                vecng<3, double> min_p;
+                vecng<3, double> max_p;
+
+                for(int i = 0; i < 3; ++i)
                 {
-                    if (v_pos[i] < min_v[i])
+                    if(p_1[i] >= p_2[i])
                     {
-                        min_v[i] = v_pos[i];
-                    }
-
-                    if (v_pos[i] > max_v[i])
+                        min_p[i] = p_2[i];
+                        max_p[i] = p_1[i];
+                    }else
                     {
-                        max_v[i] = v_pos[i];
+                        min_p[i] = p_1[i];
+                        max_p[i] = p_2[i];
                     }
                 }
+
+                c_range bx(min_p,max_p);
+                this->mesh_tree.insert(std::make_pair(bx, hl));
             }
-
-            c_range bx(pt(min_v[0], min_v[1], min_v[2]),
-                       pt(max_v[0], max_v[1], max_v[2]));
-
-            this->mesh_tree.insert(std::make_pair(bx, c_i));
         }
     }
 
@@ -58,16 +61,18 @@ namespace OasisLib
         auto min_v = bds.min_corner();
         auto max_v = bds.max_corner();
 
-        c_range bbx(pt(min_v.get<0>(), min_v.get<1>(), z),
-                    pt(max_v.get<0>(), max_v.get<1>(), z));
+        pt l_pt({min_v.get<0>(), min_v.get<1>(), z});
+        pt m_pt({max_v.get<0>(), max_v.get<1>(), z});
+
+        c_range bbx(l_pt, m_pt);
 
         this->mesh_tree.query(bgi::intersects(bbx), std::back_inserter(results));
-        Mesh m_out;
-        clip_cell(results, m_out, z);
+        //Mesh m_out;
+        //clip_cell(results, m_out, z);
 
         return results;
     }
-
+    /*
     void MeshHeightSlicer::clip_cell(vector<value> &target_facet, Mesh &m_out, double z)
     {   
         auto nb_e = target_facet.size();
@@ -93,7 +98,7 @@ namespace OasisLib
         }
 
     }
-
+    
     index_t MeshHeightSlicer::clip_cell_by_plane(index_t c_i, const Mesh *mesh, Mesh &out,double z)
     {
         vecng<3, double> p0{0.0, 0.0, z};
@@ -138,4 +143,5 @@ namespace OasisLib
         }
         return 0;
     }
+    */
 }
