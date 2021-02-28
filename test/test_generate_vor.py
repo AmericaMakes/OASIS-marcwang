874a094a2505f02a350ecf8204a0a4a3544b9952
marc wang <marc.wang@uwaterloo.ca>
2021-02-28 04:01:11 +0000

    fixed discrepeny with c++

--- test/test_generate_vor.py
+++ test/test_generate_vor.py
@@ -8,7 +8,7 @@ from OasisLib import MeshHeightSlicer
 class GeogramTest(unittest.TestCase):
 
     def setUp(self):
-        self.stl_path = './test/test_artifact/Parameter_quality_nut_1.stl'
+        self.stl_path = './test/test_artifact/3DBenchy.stl'
 
         m_in = GeoMesh()
         load_status = mesh_load(self.stl_path, m_in)
@@ -17,13 +17,14 @@ class GeogramTest(unittest.TestCase):
         mesh_repair(m_in)
 
         self.tet = GeoMesh()
-        remesh_smooth(m_in, self.tet)
+        remesh_smooth(m_in, self.tet, 10000)
 
         tetra_status = mesh_tetrahedralize(self.tet)
         self.assertTrue(tetra_status)
 
         self.voronoi = GeoMesh()
-        status_vor = polyhedral_mesher(self.tet, self.voronoi, tessallate_non_convex = False)
+        status_vor = polyhedral_mesher(self.tet, self.voronoi, 1000,
+            "tets_voronoi_boundary", 0.001, 5, 30, True, 0.0)
         self.assertTrue(status_vor)
 
         self.out_attr = MeshIOFlags()
@@ -36,8 +37,8 @@ class GeogramTest(unittest.TestCase):
     def test_single_layer(self):
         single = GeoMesh()
         self.assertTrue(self.voronoi.vertices.nb() > 0)
-        mhsl = MeshHeightSlicer(self.tet)
-        id_mapping = mhsl.get_layer(single, 10)
+        mhsl = MeshHeightSlicer(self.voronoi)
+        id_mapping = mhsl.get_layer(single, 20.0)
 
         out = './test/test_output/single_layer.obj'
         mesh_save(single, out, self.out_attr)
