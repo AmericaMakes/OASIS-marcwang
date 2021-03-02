785977824c12fe20318366253cb1994582527e72
marc wang <marc.wang@uwaterloo.ca>
2021-03-02 01:47:15 +0000

    basic slicing almost done, need to clip hatch with contour

--- oasis/slicing.py
+++ oasis/slicing.py
@@ -6,9 +6,11 @@ import trimesh
 from oasis.hatching import straight_hatch
 
 from shapely.geometry import Polygon
-from typing import Dict
+from typing import Dict, List
+from scipy.spatial import KDTree
+import random
 
-def slice_object(filename: str, layer_thickness: float):
+def slice_object(filename: str, layer_thickness: float, h_width):
 
     trimesh_inst = trimesh.load(filename)
 
@@ -28,33 +30,73 @@ def slice_object(filename: str, layer_thickness: float):
     assert(status_vor)
     bbs = trimesh_inst.extents
 
-    g = nt.Graph()
     contour_ls = []
     hatch_ls = []
-    prev_cell = {}
-    prev_slice = None
+    prev_poly = {}
+    prev_kdt = None
+    prev_g = None
     mhsl = osl.MeshHeightSlicer(voronoi)
-    for i in np.arange(0, bbs[2], layer_thickness):
+    for i in np.arange(layer_thickness, bbs[2] - layer_thickness, layer_thickness):
         single = osl.GeoMesh()
-        f2c = mhsl.get_layer(single, i)
-        c2f = dict((v,k) for k,v in f2c.items())
+        mhsl.get_layer(single, i)
+        nb_f = single.facets.nb()
+        current_poly = [convert_mesh_poly(single, f) for f in range(nb_f)]
 
-        g = update_graph(g, f2c, single)
-        g = initialize_weight(g, prev_slice, f2c, prev_cell)
+        g, kd = init_g(current_poly, prev_poly, prev_kdt, prev_g, single)
 
-        hatch_ls.append(process_hatch(g, f2c, c2f, single))
-
+        hatch_ls.append(process_hatch(g, current_poly, h_width))
         trimesh_2d_sl = trimesh_inst.section(plane_origin=[0, 0, i],
                                              plane_normal=[0, 0, 1])
 
         slice_2D, _ = trimesh_2d_sl.to_planar()
         contour_ls.append(slice_2D)
-        
-        prev_cell = f2c
-        prev_slice = single
 
-def convert_mesh_poly(mesh : osl.GeoMesh, f):
-    c_it = range(mesh.facets.corner_begin(f), mesh.facets.corner_end(f))
+        prev_poly = current_poly
+        prev_kdt = kd
+        prev_g = g
+    
+    return hatch_ls, contour_ls
+
+def init_g(current_poly: List[Polygon], prev_poly: List[Polygon],
+           prev_kdt: KDTree, prev_g: nt.Graph, mesh: osl.GeoMesh):
+    g = nt.Graph()
+    data = []
+    for f, poly in enumerate(current_poly):
+        centroid = np.array(poly.centroid.coords).squeeze()
+        data.append(centroid)
+        w, ang = compute_prev_cost_and_angle(centroid, prev_kdt, prev_poly, prev_g)
+        g.add_node(f, weight=w, angle = ang)
+
+    t = KDTree(data)
+
+    nb_f = mesh.facets.nb()
+    for f in range(nb_f):
+        c_range = range(mesh.facets.corners_begin(f),
+                        mesh.facets.corners_end(f))
+        for c in c_range:
+            f_adj = mesh.facet_corners.adjacent_facet(c)
+
+            if(f_adj != -1 and f_adj != 4294967295):
+                g.add_edge(f, f_adj)
+    return g, t
+
+
+def compute_prev_cost_and_angle(c_centroid: np.ndarray, prev_kdt: KDTree,
+                      prev_poly: List[Polygon], prev_g: nt.Graph):
+    if prev_kdt is None or prev_g is None:
+        return 0, random.uniform(0, 360)
+
+    _, f_cl = prev_kdt.query(c_centroid)
+    cost = prev_g.nodes[f_cl]['weight']
+    angle = prev_g.nodes[f_cl]['angle'] + 67
+
+    for f_adj in prev_g.adj[f_cl]:
+        cost += prev_g.nodes[f_adj]['weight'] * 0.5
+    return cost, angle
+
+
+def convert_mesh_poly(mesh: osl.GeoMesh, f):
+    c_it = range(mesh.facets.corners_begin(f), mesh.facets.corners_end(f))
 
     ring = []
     for c in c_it:
@@ -64,75 +106,31 @@ def convert_mesh_poly(mesh : osl.GeoMesh, f):
 
     return Polygon(ring)
 
-def process_hatch(g : nt.Graph, f2c: Dict[int, int], c2f: Dict[int, int],  mesh: osl.GeoMesh):
-    n_path = len(f2c)
+
+def process_hatch(g: nt.Graph, current_poly: List[Polygon], hatch : float):
     # TODO sort on subset and pop to avoid reprocessing same
-    sorted_n = sorted(g.nodes(data=True), key = lambda x : x[1]['weight'])
+    n_path = len(current_poly)
+    mod_g = g.copy()
+    sorted_n = sorted(list(mod_g.nodes(data='weight')), key = lambda x : x[1])
     min_g = sorted_n[0]
     count = 0
     hatch_order = []
-    while( count < n_path):
-        f_id = c2f[min_g[0]]
-        poly = convert_mesh_poly(mesh, f_id)
-        # TODO setting input later
-        hatch = straight_hatch(poly, 0.1, 67)
+    while(count < n_path):
+        poly = current_poly[min_g[0]]
+        angle = mod_g.nodes[min_g]['angle']
+        hatch = straight_hatch(poly, hatch, angle)
         hatch_order.append(hatch)
 
         c_area = poly.area
 
-        for c, _ in g.adj[min_g[0]]:
-            g[c]['weight'] += c_area*0.5
+        for c in g.adj[min_g[0]].keys():
+            g.nodes[c]['weight'] += c_area * 0.5
 
-        sorted_n = sorted(g.nodes(data=True), key = lambda x : x[1]['weight'])
-        min_g = sorted_n[0]
+        if len(mod_g) > 1:
+            mod_g.remove_node(min_g[0])
+            sorted_n = sorted(list(mod_g.nodes(data='weight')), key = lambda x : x[1])
+            min_g = sorted_n[0]
+        
         count += 1
-    
+
     return hatch_order
-
-def update_graph(g: nt.Graph, id_map: Dict[int, int], mesh: osl.GeoMesh):
-    facets = mesh.facets
-    for f, c in id_map.items():
-        if not g.has_node(c):
-            g.add_node(c, weight = 0)
-
-            for i in range(facets.nb_vertices(f)):
-                f_adj = facets.adjacent(f, i)
-                if f_adj != -1:
-                    g.add_edge(id_map[f_adj], c)
-    return g
-
-def compute_area_poly(mesh : osl.GeoMesh, f : int):
-    c_it = range(mesh.facets.corner_begin(f), mesh.facets.corner_end(f))
-    area_sum = 0
-
-    for c in c_it:
-        p_1_id = mesh.facet_corners.vertex(c)
-        p1 = mesh.vertices.point(p_1_id)
-
-        c_2 = mesh.facets.next_corner_around_facet(f, c)
-        p_2_id = mesh.facet_corners.vertex(c_2)
-        p2 = mesh.vertices.point(p_2_id)
-
-        area_sum += p1[0]*p2[1] - p1[1]*p2[0]
-
-    return area_sum/2
-
-def initialize_weight(g : nt.Graph, prev_layer : osl.GeoMesh, current_cell: Dict[int, int], prev_cell : Dict[int, int]):
-    if len(prev_cell) == 0 or prev_layer is None:
-        return 
-    
-    areas = {}
-    for p_f, p_c in prev_cell.items():
-        area_f = compute_area_poly(prev_layer, p_f)
-        areas[p_c] = area_f
-
-    n_g = g.copy()
-    for c in current_cell.values():
-        w = g[c]['weight']
-        for k, _ in g.adj[c].items():
-            if k in prev_cell.keys():
-                w += areas[k]*0.5
-
-        n_g[c]['weight'] = w
-
-    return n_g
